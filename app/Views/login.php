f180d58a2e9123ff73c30f40098c819254f02505
Otchi <adamgragnic@gmail.com>
2021-09-09 16:36:38 +0200

    Configuration et importation de l'index sur CodeIgniter

---
+++ app/Views/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="content/css/index.css" rel="stylesheet">
+        <title>GSB</title>
+    </head>
+
+    <body>
+
+        <!-- Formulaire de connexion-->
+
+        <form class="login" action="controller/login.php" method="post">
+            <label for="label-form">GSB - connexion</label>
+            <input type="text" placeholder="Nom d'utilisateur" name="login" autofocus required>
+            <input type="password" placeholder="Mot de passe" name="password" required>
+            <button>Entrer</button>
+        </form>
+    </body>
+</html>
